edbc043a28c777b209abf4a22ac4c39b9c49cb3f
Ishita18501 <72383952+Ishita18501@users.noreply.github.com>
2022-04-10 15:44:54 +0530

    Create hatody.js

---
+++ hatody.js
@@ -0,0 +1,9 @@
+var myAge= window. prompt("Enter your age ");
+//my Age  variable stores user input
+var earlyYears=2;
+ earlyYears = earlyYears*10.5;
+ var laterYears=myAge-2;
+ laterYears*=4
+ var myAgeInDogYears=earlyYears+laterYears;
+ var myName=window. prompt("Enter your name").toLowerCase();
+alert(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
